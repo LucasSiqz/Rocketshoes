044bb6a2fe116532be554813e87a5d260f7ffaaa
Lucas Siqueira <lucassiqzro@gmail.com>
2019-09-27 01:48:28 -0300

    Add home styles

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-6-club/88/D12-9252-388/D12-9252-388_zoom1.jpg"
+          alt=""
+        />
+        <strong>Nike mercurial</strong>
+        <span>R$129,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
