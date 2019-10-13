7641f4c717d631662d842442068414a163caf4c4
Lucas Siqueira <lucassiqzro@gmail.com>
2019-10-13 11:51:37 -0300

    Calcute total price

--- src/Pages/Cart/index.js
+++ src/Pages/Cart/index.js
@@ -7,10 +7,11 @@ import {
   MdDelete,
 } from 'react-icons/md';
 import * as CartActions from '../../store/modules/cart/actions';
+import { formatPrice } from '../../util/format';
 
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart, updateAmount }) {
+function Cart({ cart, total, removeFromCart, updateAmount }) {
   function increment(product) {
     updateAmount(product.id, product.amount + 1);
   }
@@ -51,7 +52,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
                 </div>
               </td>
               <td>
-                <strong>R$259,80</strong>
+                <strong>{product.subtotal}</strong>
               </td>
               <td>
                 <button
@@ -69,7 +70,7 @@ function Cart({ cart, removeFromCart, updateAmount }) {
         <button type="button"> Finalizar pedido</button>
         <Total>
           <span> TOTAL</span>
-          <strong>R$1290,29</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
@@ -77,7 +78,15 @@ function Cart({ cart, removeFromCart, updateAmount }) {
 }
 
 const mapStateToProps = state => ({
-  cart: state.cart,
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
 });
 
 const mapDispatchToProps = dispatch =>
